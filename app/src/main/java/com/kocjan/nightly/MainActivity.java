a23b48e0db93e85ff5634259d767804a675e68df
kocjanr <kocjanr@onid.oregonstate.edu>
2018-11-05 12:52:01 -0800

    share location button

--- app/src/main/java/com/kocjan/nightly/MainActivity.java
+++ app/src/main/java/com/kocjan/nightly/MainActivity.java
@@ -2,12 +2,23 @@ package com.kocjan.nightly;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button shareLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        shareLocation = (Button)findViewById(R.id.share_location_button);
+        shareLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
 }
